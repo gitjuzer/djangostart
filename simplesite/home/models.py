f823c473560da08d91a0499752a5f5a63fa1ccfa
ttajti <tibor.tajti@gmail.com>
2016-03-19 16:32:12 +0100

    create home model

--- simplesite/home/models.py
+++ simplesite/home/models.py
@@ -1,3 +1,12 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Content(models.Model):
+    content_text = models.CharField(max_length=2000)
+    pub_date = models.DateTimeField('date published')
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
